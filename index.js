d716d4b0d4b36c3651a2116649be22034913da8a
HASNAT <mahdi.hasnat@gmail.com>
2024-08-20 22:36:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+
+document.getElementById("search-btn").addEventListener("click", (event) => {
+    
+  const inputValue = document.getElementById("search-bar").value;
+  console.log(inputValue);
+  document.getElementById("search-bar").value = "";
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputValue}`)
+            .then(res=>res.json())
+            .then((data)=>{displayMeals(data)}
+                );
+})
+
+
+
+const displayMeals=(items)=>{
+    
+    const Maincontainer=document.getElementById("main-container");
+    Maincontainer.innerHTML="";
+    
+        items.meals.forEach((item)=>{
+            console.log(item)
+            const div=document.createElement("div");
+            div.classList.add("maincontainerdiv");
+            div.classList.add("col-lg-3","col-md-6","col-sm-12");
+            div.innerHTML=`<h1 class="food-nm" id="food nm" >${item.strMeal}</h1>
+                            <img class="food-img" id="food-img" src=${item.strMealThumb}></img>`;
+            Maincontainer.appendChild(div);
+        })
+    
+   
+        const NotFound=document.createElement('h1');
+        NotFound.textContent="Data Not Found";
+        Maincontainer.appendChild(NotFound);
+    
+   
+}
+
+
+
+
